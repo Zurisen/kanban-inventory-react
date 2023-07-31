d507256ed4a0640283b39bd20832d25f845b95cf
CMT <CMT@LAPTOP-U92UH4VT>
2023-07-31 12:55:10 +0200

    Debounced Search bar

--- src/components/Inventory/SearchBar.js
+++ src/components/Inventory/SearchBar.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState, useCallback } from 'react';
-//import { filter } from 'json-server-auth';
+import { debounce } from 'lodash';
+
 export const SearchBar = ({onChangeCallback, itemsFound}) => {
 
-    const onChange = (event) => {
+    const onChange = debounce((event) => {
         const input = event.target.value;
         onChangeCallback(input); // Pass the search input back to the Inventory component
-      };
+      }, 500);
 
     return (
         <form className="w-full">
