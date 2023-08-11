8769c30e80a1ab6265d2e138ed725881a30b1fc8
CMT <CMT@LAPTOP-U92UH4VT>
2023-08-11 08:34:51 +0200

    Revert "Revert "Revert "removed light mode"""
    This reverts commit 953b8cb9ecc1e739e80f4f8551d2fa579afb204f.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { AllRoutes } from './routes/AllRoutes';
 import {Navbar} from './components/Navbar'
 import {Sidebar} from './components/Sidebar'
 import { Toaster } from 'react-hot-toast';
-import { useState, useEffect } from 'react';
+
 function App() {
+
   return (
     <div className="app"> 
-      <div className="App h-screen bg-slate-900 dark:bg-slate-900">
+      <div className="App h-screen bg-slate-200 dark:bg-slate-900">
         <Navbar/>
         <Sidebar/>
         <Toaster/>
