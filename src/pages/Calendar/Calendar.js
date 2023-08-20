e7a696ee4761f7745c92162cdc1321ada329a976
CMT <CMT@LAPTOP-U92UH4VT>
2023-08-20 16:38:20 +0200

    added product history to calendar

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -57,6 +57,51 @@ export const Calendar = () => {
     });
   }, []);
 
+  const fetchProductHistories = async () => {
+    try {
+      const productsHistories = [];
+      const productsSnapshot = await firestore.collection('products').get();
+  
+      for (const productsDoc of productsSnapshot.docs) {
+        const productsData = productsDoc.data();
+        const serial = productsData.serial;
+        const name = productsData.name;
+  
+        // Fetch the history subcollection for each project
+        const productsHistorySnapshot = await productsDoc.ref.collection('history').get();
+        productsHistorySnapshot.forEach((historyDoc) => {
+          const historyItem = historyDoc.data();
+          const { startDate, endDate } = historyItem;
+  
+          const event = {
+            title: `[${serial}]${name} 📂${historyItem.project}`,
+            start: new Date(startDate.seconds * 1000), // Convert Firestore timestamp to JS Date
+            end: new Date(endDate.seconds * 1000),     // Convert Firestore timestamp to JS Date
+            state: historyItem.state
+          };
+  
+          productsHistories.push(event);
+        });
+      }
+  
+      setEvents(productsHistories);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchStateColors().then((data) => {
+      setStateColors(data);
+    }).catch((error) => {
+      console.error("Error fetching colors:", error);
+    });
+    
+    fetchProjectHistories().catch((error) => {
+      console.error("Error fetching histories:", error);
+    });
+  }, []);
+
   // Define a function to customize event styles
   const eventStyleGetter = (event, start, end, isSelected) => {
       const backgroundColor = stateColors[event.state];
@@ -70,7 +115,17 @@ export const Calendar = () => {
 
   return (
     <div className="p-4 sm:ml-60 bg-slate-200 dark:bg-slate-900 dark:text-white">
-      <div className="p-2 m-8 py-10 bg-slate-200 dark:bg-slate-900 dark:text-white">
+      <div className="p-4 m-8 py-10 h-24 bg-slate-200 dark:bg-slate-900 dark:text-white">
+
+      <div className="inline-flex rounded-md shadow-sm mb-5" role="group">
+        <button type="button" onClick={fetchProjectHistories} className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-l-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+          Projects View
+        </button>
+        <button type="button" onClick={fetchProductHistories} className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-r-md hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+          Products View
+        </button>
+      </div>
+
         <div>
           <BigCalendar
             localizer={localizer}
